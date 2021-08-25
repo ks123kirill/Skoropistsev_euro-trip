bde04bfa4fa123bc6633b3610b8cb20bdbe4e5ab
ks123kirill <ks123kirill@gmail.com>
2021-08-25 16:43:35 +0300

    Отредактирован блок .tabs

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,13 @@
-let mainNavJs = function () {
-  let mainHeader = document.querySelector('.main-header');
-  let navMain = mainHeader.querySelector('.main-nav');
-  let navToggle = mainHeader.querySelector('.main-nav__toggle');
+  const tabs = document.querySelector('.tabs');
+  const navTabsLink = tabs.querySelectorAll('.nav-tabs__link');
+  const listTabsItems = tabs.querySelectorAll('.list-tabs__item');
+  const catalogList = document.querySelector('.catalog__list');
+  const catalogLinks = catalogList.querySelectorAll('.catalog__link');
+
+const mainNavJs = function () {
+  const mainHeader = document.querySelector('.main-header');
+  const navMain = mainHeader.querySelector('.main-nav');
+  const navToggle = mainHeader.querySelector('.main-nav__toggle');
 
   navMain.classList.remove('main-nav--no-js');
   navMain.classList.add('main-nav--open');
@@ -17,4 +23,47 @@ let mainNavJs = function () {
   });
 }
 
+const tabsJs = function () {
+  listTabsItems.forEach((item) => {
+    if (!item.classList.contains('list-tabs__item--show')) {
+      item.classList.add('list-tabs__item--closed');
+    }
+  });
+}
+
+const getTabsFunction = function (iterator) {
+  for (let j = 0; j < navTabsLink.length; j++) {
+    navTabsLink[j].classList.remove('nav-tabs__link--active');
+    if (listTabsItems[j].classList.contains('list-tabs__item--show')) {
+      listTabsItems[j].classList.remove('list-tabs__item--show');
+      listTabsItems[j].classList.add('list-tabs__item--closed');
+    }
+  }
+  navTabsLink[iterator].classList.add('nav-tabs__link--active');
+  listTabsItems[iterator].classList.remove('list-tabs__item--closed');
+  listTabsItems[iterator].classList.add('list-tabs__item--show');
+}
+
+const getActiveTab = function () {
+  for (let i = 0; i < navTabsLink.length; i++) {
+    navTabsLink[i].addEventListener('click', function (evt) {
+      evt.preventDefault();
+      getTabsFunction(i);
+    });
+  }
+}
+
+const getCatalogLink = function () {
+  for (let i = 0; i < catalogLinks.length; i++) {
+    catalogLinks[i].addEventListener('click', function () {
+      getTabsFunction(i);
+    });
+  }
+}
+
 mainNavJs();
+tabsJs();
+getActiveTab();
+getCatalogLink();
+
+///////////
